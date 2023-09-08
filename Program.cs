bcadf0ad9b7aa4ce289c32e75e8ae41401a56c69
Firstivan <geraschenko.iv@mail.ru>
2023-09-08 20:40:36 +0300

    После смены ОСи

--- Program.cs
+++ Program.cs
@@ -3,14 +3,33 @@
 либо задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, 
 лучше обойтись исключительно массивами. 
      Примеры:
-[“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
-[“1234”, “1567”, “-2”, “computer science”] → [“-2”]
-[“Russia”, “Denmark”, “Kazan”] → []*/
+[“Hello”,   “2”,        “world”,    “:-)”]              →   [“2”, “:-)”]
+[“1234”,    “1567”,     “-2”,       “computer science”] →   [“-2”]
+[“Russia”,  “Denmark”,  “Kazan”]                        →   []*/
 
 
-void Prompt(String message)
+﻿string[] myArray = new string[7] { "I", "-233", "7777", "<3", "my", "46-7", "BF!" };
+string[] myArray2 = new string[myArray.Length];
+void newArray(string[] myArray, string[] myArray2)
 {
-    Console.WriteLine("Enter a any number: ");
+    int count = 0;
+    for (int i = 0; i < myArray.Length; i++)
+    {
+        if (myArray[i].Length <= 3)
+        {
+            myArray2[count] = myArray[i];
+            count++;
+        }
+    }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
 
-Prompt(message: );
+    Console.WriteLine();
+}
+newArray(myArray, myArray2);
+PrintArray(myArray2);
