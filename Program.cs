4f84a4ba570539d2436128cf23b9dc7ea3ad2c72
Firstivan <geraschenko.iv@mail.ru>
2023-09-26 21:12:36 +0300

    Внёс последние изменения

--- Program.cs
+++ Program.cs
@@ -8,28 +8,17 @@
 [“Russia”,  “Denmark”,  “Kazan”]                        →   []*/
 
 
-﻿string[] myArray = new string[7] { "I", "-233", "7777", "<3", "my", "46-7", "BF!" };
-string[] myArray2 = new string[myArray.Length];
-void newArray(string[] myArray, string[] myArray2)
+﻿string[] array = new string[] {"Hello", "2", "world", ":-)"};
+//string[] array = new string[] {"1234", "1567", "-2", "computer science"};
+//string[] array = new string[] {"Russia", "Denmark", "Kazan"};
+string[] res = new string[5];
+int j = 0;
+for(int i = 0; i < array.Length; i++)
 {
-    int count = 0;
-    for (int i = 0; i < myArray.Length; i++)
+    if(array[i].Length <= 3)
     {
-        if (myArray[i].Length <= 3)
-        {
-            myArray2[count] = myArray[i];
-            count++;
-        }
+        res[j] = array[i];
+        Console.Write(res[j] + "  ");
+        j = j + 1;
     }
 }
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write($"{array[i]} ");
-    }
-
-    Console.WriteLine();
-}
-newArray(myArray, myArray2);
-PrintArray(myArray2);
